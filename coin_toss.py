d4381e0fc2f3347f6aaf0572d77a7560d251cf6a
MichaelKevinCarneyInFairport <MichaelKevinCarney@gmail.com>
2025-10-02 09:10:47 +0000

    Initial Code

--- coin_toss.py
+++ coin_toss.py
@@ -1,24 +1,25 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+
+# NAME: Michael Carney
+# DATE: Wednesday October 2, 2025
+# BRIEF DESCRIPTION: 
+
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
+
 ########## ENTER YER CODE BELOW THIS LINE ##########
-
-
-
-
-
-
-
-
-
+# Don't forget to import random!!!!!
+import random
+print("===== Coin Flipper =====")
+toss = random.randint(1,2)
+if toss == 1:
+        print("Heads")
+else:
+      print("Tails")
 
 ########### END YER CODE ABOVE THIS LINE ###########
-
 
 ########################################
 #          SAMPLE OUTPUT
@@ -28,13 +29,10 @@
 Heads
 '''
 
-
-
 '''
 ===== Coin Flipper =====
 Tails
 '''
-
 
 ########################################
 #          REFLECTION QUESTIONS
@@ -42,26 +40,7 @@ Tails
 
 '''
 
-1. What was the hardest part of completing this lab? 
-
-
-
-
-
-
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
+1. What was the hardest part of completing this lab?
+   Trying to figure out how to have random choose from a sequence of strings,
+   "heads" and "tails". I couldn't figure it out.
 '''
